a712e18cc7077f8d2f3a6a1624ecef73d3bec45e
Stanislav Yatskiv <stasyatskiu2008@gmail.com>
2024-04-16 17:45:34 +0300

    new file:   life.json 	modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import tkinter as TK
 import math
+import json
 
 # var || cons
 
@@ -13,10 +14,15 @@ lastmatrics = []
 ccount = 1
 sleep = 0
 
+commands = ["random", "load"]
+
 WIDTH=600
 HEIGHT=600
 
 cellsset = set()
+
+with open('life.json') as f:
+    life = json.load(f)
 
 # main func
 
@@ -31,11 +37,39 @@ def _main() -> None:
 
     # settings
 
-    leng = int(input("len: "))
+    leng = input("len: ")
+
+    if leng.isdigit():
+        leng = int(leng)
+    else:
+        return 1
+    
     command = input("type: ")
+
+    if commands.__contains__(command):
+        pass
+    else:
+        return 2
+
     if command == "random":
-        genRandom(leng)
-    sleep = int(input("speed: "))
+        startmatrics = getRandom(leng)
+    elif command == "load":
+        command = input("name: ")
+
+        if life[command]:
+            pass
+        else:
+            return 2
+        
+        startmatrics = getEmphyMatrics(leng)
+        startmatrics = loadLife(startmatrics, life[command], leng)
+
+    sleep = input("speed: ")
+
+    if sleep.isdigit():
+        sleep = int(sleep)
+    else:
+        return 1
 
     lastmatrics = startmatrics
 
@@ -65,19 +99,19 @@ def loop():
 
     # testing tiles
 
-    getCellsSet()
-    getEmphyMatrics()
+    cellsset = getCellsSet(leng, lastmatrics)
+    matrics = getEmphyMatrics(leng)
     j = range(len(cellsset))
 
     for i in j:
         b = cellsset.pop()
         if lastmatrics[b] == 1:
-            if getCellsNum(b) == 2 or getCellsNum(b) == 3:
+            if getCellsNum(b, leng) == 2 or getCellsNum(b, leng) == 3:
                 matrics[b] = 1
             else:
                 matrics[b] = 0
         else:
-            if getCellsNum(b) == 3:
+            if getCellsNum(b, leng) == 3:
                 matrics[b] = 1
             else:
                 matrics[b] = 0
@@ -100,19 +134,38 @@ def loop():
 
 # get number of life cells arround
 
-def getCellsNum(point:int):
+def getCellsNum(point:int, leng):
     cells = 0
     j = 0
-    cCell = [
-        point - leng - 1,
-        point - leng,
-        point - leng + 1,
-        point - 1,
-        point + 1,
-        point + leng - 1,
-        point + leng,
-        point + leng + 1,
-    ]
+    cCell = []
+
+    if isOnEndOrStart(point, leng) == "end":
+        cCell =[point - leng - 1,
+                point - leng,
+                point - leng + 1 - leng,
+                point - 1,
+                point + 1 - leng,
+                point + leng - 1,
+                point + leng,
+                point + leng + 1 - leng]
+    elif isOnEndOrStart(point, leng) == "start":
+        cCell =[point - leng - 1 + leng,
+                point - leng,
+                point - leng + 1,
+                point - 1 + leng,
+                point + 1,
+                point + leng - 1 + leng,
+                point + leng,
+                point + leng + 1]
+    else:
+        cCell =[point - leng - 1,
+                point - leng,
+                point - leng + 1,
+                point - 1,
+                point + 1,
+                point + leng - 1,
+                point + leng,
+                point + leng + 1]
 
     for i in cCell:
         if i < 0:
@@ -167,35 +220,53 @@ def drawLife():
 
 # create random cells
 
-def genRandom(len):
+def getRandom(len):
     i=0
-    global startmatrics
     startmatrics = []
     for j in range(len*len):
-        i=random.randint(1,4)
+        i=random.randint(1,5)
         if i == 1:
             startmatrics.append(1)
         else:
             startmatrics.append(0)
+    return startmatrics
 
 # getting a cells to check
 
-def getCellsSet():
-    global cellsset
+def getCellsSet(leng, lastmatrics):
     cellsset = set()
     for i in range(len(lastmatrics)):
         if lastmatrics[i] == 1:
-            j = [
-                i - leng - 1,
-                i - leng,
-                i - leng + 1,
-                i - 1,
-                i,
-                i + 1,
-                i + leng - 1,
-                i + leng,
-                i + leng + 1,
-            ]
+            j = []
+
+            if isOnEndOrStart(i, leng) == "end":
+                j =[i - leng - 1,
+                        i - leng,
+                        i - leng + 1 - leng,
+                        i - 1,
+                        i + 1 - leng,
+                        i + leng - 1,
+                        i + leng,
+                        i + leng + 1 - leng]
+            elif isOnEndOrStart(i, leng) == "start":
+                j =[i - leng - 1 + leng,
+                        i - leng,
+                        i - leng + 1,
+                        i - 1 + leng,
+                        i + 1,
+                        i + leng - 1 + leng,
+                        i + leng,
+                        i + leng + 1]
+            else:
+                j =[i - leng - 1,
+                        i - leng,
+                        i - leng + 1,
+                        i - 1,
+                        i + 1,
+                        i + leng - 1,
+                        i + leng,
+                        i + leng + 1]
+
             for k in range(len(j)):
                 if j[k] < 0:
                     j[k] = j[k] + (leng*leng)
@@ -203,14 +274,41 @@ def getCellsSet():
                     j[k] = j[k] - (leng*leng)
             for k in j:
                 cellsset.add(k)
+    return cellsset
 
 # getting emphy matrics
 
-def getEmphyMatrics():
-    global matrics
+def getEmphyMatrics(leng):
     matrics = []
     for i in range(leng*leng):
         matrics.append(0)
+    return matrics
+
+# pasting life
+
+def loadLife(startmatrics, to_past, leng):
+    pos = to_past["pos"][0] + to_past["pos"][1]*leng
+    j = 0
+    k = 0
+    for i in range(len(to_past["map"])):
+        startmatrics[pos+k+(j*leng)] = to_past["map"][i]
+        if (i+1)%to_past["leng"] == 0:
+            j+=1
+            k=0
+        else:
+            k+=1
+    return startmatrics
+
+
+# end or start?
+
+def isOnEndOrStart(pointer, leng):
+    if (pointer+1)%leng == 0:
+        return "end"
+    elif (pointer+1)%leng == 1:
+        return "start"
+    return None
+
 
 # calling main
 
